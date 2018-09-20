03ce74a892d09704fdcfdaa8d9dc2bfd3a6ab576
utkrista <kcutkrista92@gmail.com>
2018-09-20 16:33:32 +0545

    create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,13 @@
+var expect = require('expect');
+var { generateMessage } = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate correct message object', () => {
+    var from = 'utkrista';
+    var text = 'some message';
+    var message = generateMessage(from, text);
+
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toMatchObject({ from, text });
+  });
+});
